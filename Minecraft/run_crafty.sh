7a0fdf782a534e32010d995571f869af85326c0f
tassog507-cpu <255418173+tassog507-cpu@users.noreply.github.com>
2026-02-23 19:35:54 +0000

    Atualizando arquivos do instalador

---
+++ Minecraft/run_crafty.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /workspaces/Servidor/Minecraft
+source .venv/bin/activate 
+cd crafty-4 
+exec python3 main.py 
---
+++ Minecraft/run_crafty_service.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /workspaces/Servidor/Minecraft
+source .venv/bin/activate 
+cd crafty-4 
+python3 main.py -d
